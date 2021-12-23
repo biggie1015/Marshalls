06249d65a4331cbb091eef3a5a400bc019cb2588
ismel Santana <isantana@oriontek.do>
2021-12-23 01:51:45 -0400

    Changes about Structure

---
+++ Marshall.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Marshall.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Marshall.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Marshall.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
